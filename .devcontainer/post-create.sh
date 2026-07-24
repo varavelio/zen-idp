ce9a5e6c3002de07866037452490423b26e81f52
Luis Eduardo <eduardo.devop@gmail.com>
2026-07-23 21:36:41 -0600

    chore: add devcontainer configuration for Rust development

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Create the root's .bashrc
+cat << 'EOF' >> /root/.bashrc
+
+# Define command aliases
+alias ll='ls -alh'
+alias c='clear'
+alias t='task'
+alias n='npm'
+
+# Set the user file-creation mode mask to 000
+umask 000
+
+EOF
+
+# Configure file permissions for the /workspaces directory
+chmod -R 777 /workspaces
+
+# Configure git
+git config core.fileMode false
