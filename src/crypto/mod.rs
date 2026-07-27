d298858c86e45bf09acdffb26ccaf30ac75a5224
Luis Eduardo <eduardo.devop@gmail.com>
2026-07-27 00:02:04 -0600

    chore: remove Rust implementation and toolchain configuration

--- src/crypto/mod.rs
+++
@@ -1,5 +0,0 @@
-//! Cryptographic helpers used by zen identity provider.
-
-mod root_secret;
-
-pub(crate) use root_secret::generate_root_secret;
--- src/main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("Zen IdP");
-}
